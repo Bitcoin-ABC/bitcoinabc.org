9f1a4d5020ba1e53d9f8a118c6a7a1808803a4bf
Jason B. Cox <contact@jasonbcox.com>
2020-09-01 11:36:12 -0700

    Fix blog feed ordering
    Summary: The blog feed does not need to be reversed.
    
    Test Plan:
    `make serve`
    Navigate to http://localhost:4000/
    
    Reviewers: #bitcoin_abc, Fabien
    
    Reviewed By: #bitcoin_abc, Fabien
    
    Differential Revision: https://reviews.bitcoinabc.org/D7298

--- js/blog-feed.js
+++ js/blog-feed.js
@@ -1,7 +1,7 @@
 $.getJSON("https://blog.bitcoinabc.org/json/", function (data) {
   var feed = $(".abc-blog-feed");
   feed.empty();
-  $.each(data["items"].reverse().slice(0, 3), function (i, post) {
+  $.each(data["items"].slice(0, 3), function (i, post) {
     var encodedURL = encodeURI(post.url);
     feed.append(
       $('<article class="post-preview" />').append(
